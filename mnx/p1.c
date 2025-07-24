f635b950a326a4522d5ee4374287857158758fbf
MANAS JOSHI <158481955+itsmnx@users.noreply.github.com>
2025-07-24 09:22:58 +0530

    Add files via upload

---
+++ mnx/p1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+pid_t child_pid;
+child_pid=fork();
+
+if(child_pid==0){
+printf("Child process: PID=%d\n",getpid());
+}
+else if(child_pid>0){
+printf("Parent Process: PID=%d, Child Pid=%d\n",getpid(),child_pid);
+}
+else{
+perror("fork failed");
+}
+return 0;
+}
+	
