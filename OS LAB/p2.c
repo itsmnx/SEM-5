5ed4497ec2f916eb038b799995eae0c932e3eacb
MANAS JOSHI <158481955+itsmnx@users.noreply.github.com>
2025-11-04 10:55:27 +0530

    Named Pipe practical no.-9

---
+++ OS LAB/p2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+   printf("\n\t\t\t\t\tNAME: MANAS JOSHI| SECTION: C (G1) | ROLL NUMBER: 37\n");
+   int fd;
+   char *myfifo = "/tmp/myfifo";
+
+   mkfifo(myfifo, 0666);
+
+   char arr1[80], arr2[80];
+   int bytesWritten, bytesRead;
+
+   while (1)
+   {
+      fd = open(myfifo, O_WRONLY);
+      fgets(arr2, 80, stdin);
+      bytesWritten = write(fd, arr2, strlen(arr2) + 1);
+      printf("Bytes written: %d\n", bytesWritten);
+      close(fd);
+
+      fd = open(myfifo, O_RDONLY);
+      bytesRead = read(fd, arr1, sizeof(arr1));
+      printf("USER 2: %s", arr1);
+      printf("Bytes read: %d\n", bytesRead);
+      close(fd);
+   }
+   return 0;
+}
---
+++ OS LAB/p3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("\n\t\t\t\t\tNAME: MANAS JOSHI | SECTION: C (G1) | ROLL NUMBER: 37\n");
+    int fd;
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
+
+    char str1[80], str2[80];
+    int bytesRead, bytesWritten;
+
+    while (1)
+    {
+        fd = open(myfifo, O_RDONLY);
+        bytesRead = read(fd, str1, 80);       
+        printf("User1: %s", str1);
+        printf("Bytes read: %d\n", bytesRead);
+        close(fd);
+
+        fd = open(myfifo, O_WRONLY);
+        fgets(str2, 80, stdin);
+        bytesWritten = write(fd, str2, strlen(str2) + 1);
+        printf("Bytes written: %d\n", bytesWritten);
+        close(fd);
+    }
+
+    return 0;
+}
